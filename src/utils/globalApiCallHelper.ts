9736ce0fc1701c059cb0d515c8fc927a896fbad1
Zaky <zak786khan@gmail.com>
2023-07-07 17:17:08 +0530

    Fix:API

--- src/utils/globalApiCallHelper.ts
+++ src/utils/globalApiCallHelper.ts
@@ -9,7 +9,7 @@ type GlobalApiCallHelperProps = {
 
 const baseURL =
   process.env.NEXT_PUBLIC_API_BASE_URL !== undefined
-    ? process.env.NEXT_PUBLIC_API_BASE_URL !== undefined
+    ? process.env.NEXT_PUBLIC_API_BASE_URL
     : "http://localhost:5000/api";
 
 export const globalApiCallHelper = async ({
